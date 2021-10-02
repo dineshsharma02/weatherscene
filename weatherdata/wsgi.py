f2750212927af1d910620f4e21fc7a09f0964a2a
Dinesh <itsdinesh02@gmail.com>
2021-10-02 14:38:46 +0530

    config for heroku

--- weatherdata/wsgi.py
+++ weatherdata/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 # from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weatherdata.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weatherdata.settings.base')
 
 
 application = get_wsgi_application()
