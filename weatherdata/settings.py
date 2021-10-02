fef6d4cd8516c25e6d25f59684f4120ef8bfc8b8
Dinesh <itsdinesh02@gmail.com>
2021-10-02 14:20:24 +0530

    deploy 2

--- weatherdata/settings.py
+++ weatherdata/settings.py
@@ -10,30 +10,30 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-import environ
+# import environ
 import django_heroku
 from pathlib import Path
-import dj_database_url
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+# env = environ.Env(
+#     # set casting, default value
+#     DEBUG=(bool, False)
+# )
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-l4b@iz87@5+xy3thu&2kb=47=q&l34+rljz6&jz4$a*8!rg1w+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['127.0.0.1','weatherscene.herokuapp.com']
 
 
 
@@ -83,13 +83,11 @@ WSGI_APPLICATION = 'weatherdata.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': 5432,
+        
     }
+
 }
+
 # db_from_env = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(db_from_env)
 
